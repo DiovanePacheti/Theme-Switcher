299a8f423cccf7212b3cf3316c8dabb67d2fed24
DiovanePacheti <frontendestudo@gmail.com>
2020-09-14 20:06:54 -0300

    subindo project

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,10 @@
+export default{
+    title:'dark',
+
+    colors:{
+        primary:'#333',
+        secundary:'#BA324F',
+        background:'#222',
+        text:'#fff'
+    }
+}
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,10 @@
+export default{
+    title:'light',
+
+    colors:{
+        primary:'#175676',
+        secundary:'#BA324F',
+        background:'#f5f5f5',
+        text:'#333'
+    }
+}
